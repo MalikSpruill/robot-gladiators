53650d9efb9e6048710acaef4ac516f6479a684f
Malik <malikspruill@gmail.com>
2022-03-03 01:03:53 -0500

    Fixed syntax error in player object

--- assets/js/game.js
+++ assets/js/game.js
@@ -164,7 +164,7 @@ var randomNumber = function(min, max) {
 }
 
 var playerInfo = {
-  name: window.prompt("What is your robot's name?");
+  name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
   money: 10,
