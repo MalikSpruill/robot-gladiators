811b3b65c38fa7349ed3d537171b294ef5543959
Malik <malikspruill@gmail.com>
2022-03-04 17:20:28 -0500

    Added highscore functionality to save progress in local storage with web api

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var fightOrSkip = function() {
   // ask player if they'd like to fight or skip using fightOrSkip function
-  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+  var promptFight = 
+  ('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   promptFight = promptFight.toLowerCase();
 
@@ -127,7 +128,7 @@ var startGame = function () {
     fight(pickedEnemyObj); 
 
     // if player is still alive and we're not at the last enemy in the array
-    if (playerInfo.health > 0 && i < enemy.names.length - 1) {
+    if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
       // ask if player wants to use the store before next round
       var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
 
@@ -148,21 +149,30 @@ var startGame = function () {
 
 // function to end the entire game
 var endGame = function() {
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
   }
+  // if player has more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
   else {
-    window.alert("You've lost your robot in battle.");
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
   if (playAgainConfirm) {
-    // restart the game
     startGame();
-  }
+  } 
   else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
